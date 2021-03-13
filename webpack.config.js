5422e864c8cf0f5ab313626ae5b741dd075d423d
ecDusty <e.macalligan@outlook.com>
2021-03-13 23:13:31 +0800

    add: working through step 3

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
+    mode: 'development',
     entry: './src/client/index.js',
     devtool: 'source-map',
     module: {
